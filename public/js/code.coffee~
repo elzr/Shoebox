d796f4d1cadb5e06a32de48dc30856069aa64d92
Xian Melia <type.heroku@gmail.com>
2011-09-02 00:27:42 -0500

    dragging works!

--- public/js/code.coffee~
+++ public/js/code.coffee~
@@ -1,57 +1,53 @@
 window.SHOE = SHOE =
-  rand:()->.5-Math.random()
   images:
-    total:30,
-    get: (set=1341466) ->
+    total:10, count:0,
+    piles:[{x:.25, y:.4}, {x:.75, y:.25}, {x:.6, y:.75}]
+
+    fetch: (set=1341466) ->
       $.getJSON('http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=a948a36e48c16afbf95a03c85418f417&photoset_id='+
         set+'&format=json&extras=url_s&jsoncallback=?', SHOE.images.display)
-    place: (img, pile, radius=200) ->
+    pile: (i) ->
+      this.piles[Math.floor(i/(this.total/3))]
+    place: (img, count, radius=200) ->
       limit = {x:$('#canvas').width(), y:$('#canvas').height()}
-      pile = {x:limit.x*pile.x, y:limit.y*pile.y}
+      pile = this.pile(count) #pile of relative values
+      pile = {x:limit.x*pile.x, y:limit.y*pile.y} # pile of concrete values
       img.css(
-        left:Math.min(limit.x, Math.max(0, pile.x+(radius*SHOE.rand())-img.outerWidth()/2) ),
-        top:Math.min(limit.y, Math.max(0, pile.y+(radius*SHOE.rand())-img.outerHeight()/2) )
+        left:Math.min(limit.x, Math.max(0, pile.x+(radius*U.rand())-img.outerWidth()/2) ),
+        top:Math.min(limit.y, Math.max(0, pile.y+(radius*U.rand())-img.outerHeight()/2) )
       )
+    build: () ->
+      a = $(this).parents('a'); img = $(this)
+      SHOE.moko = img
+      a.data('rotation',30*U.rand()).rotate(a.data('rotation')+'deg')
+      a.css(
+        height:img.outerHeight()
+        width:img.outerWidth()
+      )
+      SHOE.images.place(a, SHOE.images.count++)
     display: (data) ->
+      data = data.photoset.photo.sort( ()->U.rand() ).slice(0, SHOE.images.total)
       $('#shoebox p').remove()
-      _(data.photoset.photo.sort( () -> .5-Math.random() )).chain().
-        first(SHOE.images.total).
-        each( (value, key, list) ->
-          $('#canvas').append('<a href="javascript:void(0)"><img src="'+value.url_s+'" /><span>'+value.title+'</span></a>')
-        )
-      $('#canvas a').each( () ->
-          $(this).data('rotation',30*SHOE.rand()).rotate($(this).data('rotation'))
-        ).
-        click(() ->
+      for item in data
+        $('#canvas').append('<a href="javascript:void(0)"><img src="'+item.url_s+'" /><span>'+item.title+'</span></a>')
+      $('#canvas a').hover(
+          ()->$(this).animate({rotate:0}, 450),
+          ()->$(this).animate({rotate:$(this).data('rotation')}, 450)
+        ).dblclick(() ->
           that = $(this)
-          that.flip(
-            direction: if that.hasClass('backside') then 'rl' else 'lr'
-            speed:400
-            onAnimation: () -> that.toggleClass('backside')
-          )
-          
-        ).find('img').load( () ->
-          $(this).parent().css(
-            height:$(this).outerHeight()
-            width:$(this).outerWidth()
-          )
-          count = Math.floor((--SHOE.images.total)/10)
-          pile = [{x:.25, y:.4}, {x:.75, y:.25}, {x:.6, y:.75}][count]
-          SHOE.images.place($(this).parents('a'), pile)
-        )
-      otherFlip: (that) ->
-        that.hover(
-          ()->$(this).rotate(animateTo:0, duration:300),
-          ()->$(this).rotate(animateTo:$(this).data('rotation'), duration:300)
-        ).click( ()->
-          flip = if that.hasClass('backside') then 'unflip' else 'flip'
-          that.rotate3Di(flip, 700,
-            sideChange: (front) ->
+          that.rotate3Di('toggle', 700,
+            sideChange: () ->
               that.toggleClass('backside')
-              that.data('front',front)
           )
-        )
+        ).find('img').load( SHOE.images.build )
+      $(window).load( ()-> $('#canvas a').draggable(
+        containment:'parent'
+        stop: ()-> $(this).css('z-index',3)
+      ))
+U =
+  rand:()->.5-Math.random()
+  print:(obj)-> '{'+("#{key}:#{if $.isPlainObject(value) then U.print(value) else value}" for key, value of obj).join(', ')+'}'
 
 $( () ->
-  SHOE.images.get()
+  SHOE.images.fetch()
 )
--- public/js/code.js
+++ public/js/code.js
@@ -1,23 +1,36 @@
-/* DO NOT MODIFY. This file was compiled Fri, 02 Sep 2011 02:47:39 GMT from
+/* DO NOT MODIFY. This file was compiled Fri, 02 Sep 2011 05:26:22 GMT from
  * /Users/sam/projects/sinatra/shoebox/public/js/code.coffee
  */
 
 (function() {
-  var SHOE;
+  var SHOE, U;
   window.SHOE = SHOE = {
-    rand: function() {
-      return .5 - Math.random();
-    },
     images: {
-      total: 30,
-      get: function(set) {
+      total: 10,
+      count: 0,
+      piles: [
+        {
+          x: .25,
+          y: .4
+        }, {
+          x: .75,
+          y: .25
+        }, {
+          x: .6,
+          y: .75
+        }
+      ],
+      fetch: function(set) {
         if (set == null) {
           set = 1341466;
         }
         return $.getJSON('http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=a948a36e48c16afbf95a03c85418f417&photoset_id=' + set + '&format=json&extras=url_s&jsoncallback=?', SHOE.images.display);
       },
-      place: function(img, pile, radius) {
-        var limit;
+      pile: function(i) {
+        return this.piles[Math.floor(i / (this.total / 3))];
+      },
+      place: function(img, count, radius) {
+        var limit, pile;
         if (radius == null) {
           radius = 200;
         }
@@ -25,83 +38,84 @@
           x: $('#canvas').width(),
           y: $('#canvas').height()
         };
+        pile = this.pile(count);
         pile = {
           x: limit.x * pile.x,
           y: limit.y * pile.y
         };
         return img.css({
-          left: Math.min(limit.x, Math.max(0, pile.x + (radius * SHOE.rand()) - img.outerWidth() / 2)),
-          top: Math.min(limit.y, Math.max(0, pile.y + (radius * SHOE.rand()) - img.outerHeight() / 2))
+          left: Math.min(limit.x, Math.max(0, pile.x + (radius * U.rand()) - img.outerWidth() / 2)),
+          top: Math.min(limit.y, Math.max(0, pile.y + (radius * U.rand()) - img.outerHeight() / 2))
         });
       },
-      display: function(data) {
-        $('#shoebox p').remove();
-        _(data.photoset.photo.sort(function() {
-          return .5 - Math.random();
-        })).chain().first(SHOE.images.total).each(function(value, key, list) {
-          return $('#canvas').append('<a href="javascript:void(0)"><img src="' + value.url_s + '" /><span>' + value.title + '</span></a>');
+      build: function() {
+        var a, img;
+        a = $(this).parents('a');
+        img = $(this);
+        SHOE.moko = img;
+        a.data('rotation', 30 * U.rand()).rotate(a.data('rotation') + 'deg');
+        a.css({
+          height: img.outerHeight(),
+          width: img.outerWidth()
         });
-        $('#canvas a').each(function() {
-          return $(this).data('rotation', 30 * SHOE.rand()).rotate($(this).data('rotation'));
-        }).click(function() {
+        return SHOE.images.place(a, SHOE.images.count++);
+      },
+      display: function(data) {
+        var item, _i, _len;
+        data = data.photoset.photo.sort(function() {
+          return U.rand();
+        }).slice(0, SHOE.images.total);
+        $('#shoebox p').remove();
+        for (_i = 0, _len = data.length; _i < _len; _i++) {
+          item = data[_i];
+          $('#canvas').append('<a href="javascript:void(0)"><img src="' + item.url_s + '" /><span>' + item.title + '</span></a>');
+        }
+        $('#canvas a').hover(function() {
+          return $(this).animate({
+            rotate: 0
+          }, 450);
+        }, function() {
+          return $(this).animate({
+            rotate: $(this).data('rotation')
+          }, 450);
+        }).dblclick(function() {
           var that;
           that = $(this);
-          return that.flip({
-            direction: 'lr',
-            speed: 400,
-            onAnimation: function() {
+          return that.rotate3Di('toggle', 700, {
+            sideChange: function() {
               return that.toggleClass('backside');
             }
           });
-        }).find('img').load(function() {
-          var count, pile;
-          $(this).parent().css({
-            height: $(this).outerHeight(),
-            width: $(this).outerWidth()
-          });
-          count = Math.floor((--SHOE.images.total) / 10);
-          pile = [
-            {
-              x: .25,
-              y: .4
-            }, {
-              x: .75,
-              y: .25
-            }, {
-              x: .6,
-              y: .75
+        }).find('img').load(SHOE.images.build);
+        return $(window).load(function() {
+          return $('#canvas a').draggable({
+            containment: 'parent',
+            stop: function() {
+              return $(this).css('z-index', 3);
             }
-          ][count];
-          return SHOE.images.place($(this).parents('a'), pile);
+          });
         });
-        return {
-          otherFlip: function(that) {
-            return that.hover(function() {
-              return $(this).rotate({
-                animateTo: 0,
-                duration: 300
-              });
-            }, function() {
-              return $(this).rotate({
-                animateTo: $(this).data('rotation'),
-                duration: 300
-              });
-            }).click(function() {
-              var flip;
-              flip = that.hasClass('backside') ? 'unflip' : 'flip';
-              return that.rotate3Di(flip, 700, {
-                sideChange: function(front) {
-                  that.toggleClass('backside');
-                  return that.data('front', front);
-                }
-              });
-            });
-          }
-        };
       }
     }
   };
+  U = {
+    rand: function() {
+      return .5 - Math.random();
+    },
+    print: function(obj) {
+      var key, value;
+      return '{' + ((function() {
+        var _results;
+        _results = [];
+        for (key in obj) {
+          value = obj[key];
+          _results.push("" + key + ":" + ($.isPlainObject(value) ? U.print(value) : value));
+        }
+        return _results;
+      })()).join(', ') + '}';
+    }
+  };
   $(function() {
-    return SHOE.images.get();
+    return SHOE.images.fetch();
   });
 }).call(this);
